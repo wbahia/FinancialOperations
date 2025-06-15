467f2250b4a551607badf3e46fa9f30b25e37d7c
Walter Bahia <wbahia@gmail.com>
2025-06-15 12:21:07 -0300

    Adicionando Infrastructure Layer

---
+++ FinancialOperations.Infrastructure/Repositories/InMemoryAccountRepository.cs
@@ -0,0 +1,39 @@
+﻿using FinancialOperations.Application.Interfaces;
+using FinancialOperations.Domain.Entities;
+using System.Collections.Concurrent;
+
+namespace FinancialOperations.Infrastructure.Repositories;
+
+public class InMemoryAccountRepository : IAccountRepository
+{
+    private readonly ConcurrentDictionary<Guid, Account> _accounts = new();
+
+    public Task<Account?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default)
+    {
+        _accounts.TryGetValue(id, out var account);
+        return Task.FromResult(account);
+    }
+
+    public Task<Account> AddAsync(Account entity, CancellationToken cancellationToken = default)
+    {
+        _accounts.TryAdd(entity.Id, entity);
+        return Task.FromResult(entity);
+    }
+
+    public Task UpdateAsync(Account entity, CancellationToken cancellationToken = default)
+    {
+        _accounts.TryUpdate(entity.Id, entity, _accounts[entity.Id]);
+        return Task.CompletedTask;
+    }
+
+    public Task<IEnumerable<Account>> GetAllAsync(CancellationToken cancellationToken = default)
+    {
+        return Task.FromResult(_accounts.Values.AsEnumerable());
+    }
+
+    public Task<IEnumerable<Account>> GetByCustomerIdAsync(Guid customerId, CancellationToken cancellationToken = default)
+    {
+        var accounts = _accounts.Values.Where(a => a.CustomerId == customerId);
+        return Task.FromResult(accounts);
+    }
+}
---
+++ FinancialOperations.Infrastructure/Repositories/InMemoryCustomerRepository.cs
@@ -0,0 +1,39 @@
+﻿using FinancialOperations.Application.Interfaces;
+using FinancialOperations.Domain.Entities;
+using System.Collections.Concurrent;
+
+namespace FinancialOperations.Infrastructure.Repositories;
+
+public class InMemoryCustomerRepository : ICustomerRepository
+{
+    private readonly ConcurrentDictionary<Guid, Customer> _customers = new();
+
+    public Task<Customer?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default)
+    {
+        _customers.TryGetValue(id, out var customer);
+        return Task.FromResult(customer);
+    }
+
+    public Task<Customer> AddAsync(Customer entity, CancellationToken cancellationToken = default)
+    {
+        _customers.TryAdd(entity.Id, entity);
+        return Task.FromResult(entity);
+    }
+
+    public Task UpdateAsync(Customer entity, CancellationToken cancellationToken = default)
+    {
+        _customers.TryUpdate(entity.Id, entity, _customers[entity.Id]);
+        return Task.CompletedTask;
+    }
+
+    public Task<IEnumerable<Customer>> GetAllAsync(CancellationToken cancellationToken = default)
+    {
+        return Task.FromResult(_customers.Values.AsEnumerable());
+    }
+
+    public Task<Customer?> GetByDocumentAsync(string document, CancellationToken cancellationToken = default)
+    {
+        var customer = _customers.Values.FirstOrDefault(c => c.Document == document);
+        return Task.FromResult(customer);
+    }
+}
